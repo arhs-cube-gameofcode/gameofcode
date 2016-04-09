65aaf32ec1e7004dba918c89d74c8159fe9cb209
Mathieu Colin <matt54425+Assembla.com@gmail.com>
2016-04-09 02:59:15 +0200

    Add watch on the location criteria.

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -5,6 +5,14 @@ angular.module('thatsmyspotApp')
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+            // Criteria
+            $scope.$watch('criteria.location', function(newValue, oldValue) {
+                if(newValue) {
+                    //TODO call the map centering function.
+                }
+            });
+
+            // Map
             var attr_osm = 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
                 attr_overpass = 'POI via <a href="http://www.overpass-api.de/">Overpass API</a>';
             var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {opacity: 0.7, attribution: [attr_osm, attr_overpass].join(', ')});
