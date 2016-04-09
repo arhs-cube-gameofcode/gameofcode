6adf3836133c30d8c0ee6c26a7046266f45d8a0c
borellda <daniel.borella@arhs-cube.com>
2016-04-09 02:36:37 +0200

    working leaflet with overpass for hotels

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -5,5 +5,18 @@ angular.module('thatsmyspotApp')
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
+            var attr_osm = 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
+                attr_overpass = 'POI via <a href="http://www.overpass-api.de/">Overpass API</a>';
+            var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {opacity: 0.7, attribution: [attr_osm, attr_overpass].join(', ')});
+
+            var map = new L.Map('mapid').addLayer(osm).setView(new L.LatLng(52.265, 10.524), 14);
+
+        //OverPassAPI overlay
+            var opl = new L.OverPassLayer({
+                query: "node(BBOX)['tourism'='hotel'];out;",
+            });
+
+            map.addLayer(opl);
+
         });
     });
