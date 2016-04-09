c937c9515bbbdb7694699b2b5917e5d2f8832314
Mathieu Colin <matt54425+Assembla.com@gmail.com>
2016-04-09 17:23:28 +0200

    Bug fix.

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -84,15 +84,15 @@ angular.module('thatsmyspotApp')
             });
 
             $scope.getDistanceFromLatLonInKm = function(lat1,lon1,lat2,lon2) {
-            	  var R = 6371; 
+            	  var R = 6371;
             	  var dLat = $scope.deg2rad(lat2-lat1);
-            	  var dLon = $scope.deg2rad(lon2-lon1); 
-            	  var a = 
+            	  var dLon = $scope.deg2rad(lon2-lon1);
+            	  var a =
             	    Math.sin(dLat/2) * Math.sin(dLat/2) +
-            	    Math.cos($scope.deg2rad(lat1)) * Math.cos($scope.deg2rad(lat2)) * 
+            	    Math.cos($scope.deg2rad(lat1)) * Math.cos($scope.deg2rad(lat2)) *
             	    Math.sin(dLon/2) * Math.sin(dLon/2)
-            	    ; 
-            	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            	    ;
+            	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
             	  var d = R * c; // Distance in km
             	  return d;
             	}
@@ -100,21 +100,29 @@ angular.module('thatsmyspotApp')
             $scope.deg2rad = function(deg) {
             	  return deg * (Math.PI/180)
             }
-            
-            $scope.chooseAddr = function(lat, lng){
-            	  var location = new L.LatLng(lat, lng);
+
+            $scope.chooseAddr = function(value){
+                var lat = value.lat;
+                var lng = value.lon;
+
+                //Update search criteria
+                $scope.criteria.location = value.display_name;
+                $scope.searchRes = [];
+
+
+                var location = new L.LatLng(lat, lng);
             	  map.panTo(location);
             	  map.setZoom(25);
             	  var fixedMarker = L.marker(location).bindPopup('Location').addTo(map);
-            	  
+
                   $http.get('/api/routes?lat=' + lat + '&lng=' + lng + '&distance=' + 150).then(function(response) {
                 	  console.log(response.data);
-                	  
+
                 	  response.data.forEach(function(bussLine){
                 		  //TODO when real data, calculate the closest station relative to lat, lng
-                		  
+
                 		  bussLine.inDist = false;
-                		  
+
                 		  bussLine.stations.forEach(function(station){
                 			  var diff = $scope.getDistanceFromLatLonInKm(lat, lng, station.lat, station.lng) * 1000;
                 			  //make 300 configurable
@@ -123,27 +131,27 @@ angular.module('thatsmyspotApp')
                 				  L.marker(new L.LatLng(station.lat, station.lng), {
                 				        icon: $scope.north
                 			      }).bindPopup('Bus id:' + bussLine.id).addTo(map);
-                				  
+
                 				  bussLine.inDist = true;
                 			  }
                 		  })
-                		  
+
                 		  if(bussLine.inDist == true){
                 			  var coord = $scope.calculateRoutes(lat, lng, bussLine.stations, 2500);
-                			  
+
                 			  var c = 2;
                 			  for(c in coord){
                     			  L.marker(new L.LatLng(coord[c].lat, coord[c].lng), {
                 				        icon: $scope.northSmall
                 			      }).addTo(map);
                 			  }
-                			  
+
                 			  L.polyline(coord, {color: 'red'}).addTo(map);
                 		  }
                 	  })
-                  });  
+                  });
             }
-            
+
             $scope.north = L.icon({
                 iconUrl: '//esri.github.io/esri-leaflet/img/bus-stop-north.png',
                 iconRetinaUrl: '//esri.github.io/esri-leaflet/img/bus-stop-north@2x.png',
@@ -151,35 +159,35 @@ angular.module('thatsmyspotApp')
                 iconAnchor: [13.5, 17.5],
                 popupAnchor: [0, -11],
               })
-             
+
             $scope.northSmall = L.icon({
                 iconUrl: '//esri.github.io/esri-leaflet/img/bus-stop-north.png',
                 iconRetinaUrl: '//esri.github.io/esri-leaflet/img/bus-stop-north@2x.png',
                 iconSize: [14, 16],
                 iconAnchor: [13.5, 17.5],
                 popupAnchor: [0, -11],
-              })              
-              
+              })
+
             $scope.calculateRoutes = function (lat, lng, stations, limitDistance){
                 var pathCoords = [];
                 var dist = 0;
                 for(var i in stations) {
                     var x = stations[i].lat;
                     var y = stations[i].lng;
-                   
+
                     dist+=$scope.getDistanceFromLatLonInKm(lat, lng, x, y) * 1000;
-                    
+
                     if(dist < limitDistance){
                     	pathCoords.push(new L.LatLng(x, y));
-                        
+
                         lat = x;
                         lng = y;
                     } else break;
                 }
- 
+
                 return pathCoords;
-            }  
-            
+            }
+
 
             // Criteria distance
             $scope.$watch('criteria.distance', function(newValue, oldValue) {
