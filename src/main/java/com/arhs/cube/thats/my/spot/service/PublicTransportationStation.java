0d12031febb2ae2d0c123533e7128d4e1806fb0f
borellda <daniel.borella@arhs-cube.com>
2016-04-18 09:35:15 +0200

    add doc

--- src/main/java/com/arhs/cube/thats/my/spot/service/PublicTransportationStation.java
+++ src/main/java/com/arhs/cube/thats/my/spot/service/PublicTransportationStation.java
@@ -1,6 +1,7 @@
 package com.arhs.cube.thats.my.spot.service;
 
 import com.arhs.cube.thats.my.spot.service.util.Station2LineWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * Created by borellda on 4/9/2016.
@@ -35,6 +39,13 @@ public class PublicTransportationStation {
 
         HttpEntity<String> response = restTemplate.exchange(builder.build().encode().toUri(), HttpMethod.GET, entity, String.class);
 
+        HashMap<String,Object> result = new HashMap<>( );
+        try {
+            result =
+                new ObjectMapper().readValue(response.getBody(), HashMap.class);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
         log.info(String.format("Result :%s", response.getBody()));
 
     }
